80939a5023da2c7ac4ee8e7326f7bcf9bb99a63c
muhamadgibran <muhamadgibran751@gmail.com>
2024-07-29 14:23:59 +0700

    init project

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize')
+
+/** SQL Connectiion */
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  //port: process.env.DB_PORT,
+  dialect: "mysql",
+  logging: false,
+  multipleStatements: true,
+})
+
+try {
+  db.authenticate()
+  console.log('Database Connected!')
+  db.sync()
+} catch (err) {
+  console.error(err)
+}
+
+module.exports = db
